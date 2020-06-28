d1659eedce69baf0560c694e80103b9769c6defd
Itacir Gabral <itacirgabral@gmail.com>
2020-06-27 22:51:09 -0400

    bugged 1

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,42 @@
 //!
 #![warn(missing_debug_implementations, rust_2018_idioms, missing_docs)]
 
-pub struct StrSplit {
-
+#[derive(Debug)]
+pub struct StrSplit<'a> {
+    remainder: &'a str,
+    delimiter: &'a str,
 }
 
-impl StrSplit {
-    pub fn new(haystack: &str, delimiter: &str) -> Self {
-
+impl<'a> StrSplit<'a> {
+    pub fn new(haystack: &'a str, delimiter: &'a str) -> Self {
+        Self {
+            remainder: haystack,
+            delimiter,
+        }
     }
 }
 
-impl Iterator for StrSplit {
-    type Item = &str;
+impl<'a> Iterator for StrSplit<'a> {
+    type Item = &'a str;
     fn next(&mut self) -> Option<Self::Item> {
-
+        if let Some(next_delimiter) = self.remainder.find(self.delimiter) {
+            let until_delimiter = &self.remainder[..next_delimiter];
+            self.remainder = &self.remainder[(next_delimiter + self.delimiter.len())..];
+            Some(until_delimiter)
+        } else if self.remainder.is_empty() {
+            None
+        } else {
+            let rest = self.remainder;
+            self.remainder = ""; 
+            Some(rest)
+        }
     }
 }
 
 #[test]
 fn it_works() {
     let haystack = "a b c d e";
-    let letters = StrSplit::new(haystack, " ");
-    let gauge = vec!["a", "b", "c", "d", "e"].into_iter();
+    let letters: Vec<&str> = StrSplit::new(haystack, " ").collect();
+    let gauge = vec!["a", "b", "c", "d", "e"];
     assert_eq!(letters, gauge);
 }
