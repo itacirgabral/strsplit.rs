588462f7fe4a848a5854eac7aced8cefabdcf270
Itacir Gabral <itacirgabral@gmail.com>
2020-06-28 16:06:05 -0400

    two lifetime

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,13 @@
 #![warn(missing_debug_implementations, rust_2018_idioms, missing_docs)]
 
 #[derive(Debug)]
-pub struct StrSplit<'a> {
-    remainder: Option<&'a str>,
-    delimiter: &'a str,
+pub struct StrSplit<'haystack, 'delimiter> {
+    remainder: Option<&'haystack str>,
+    delimiter: &'delimiter str,
 }
 
-impl<'a> StrSplit<'a> {
-    pub fn new(haystack: &'a str, delimiter: &'a str) -> Self {
+impl<'haystack, 'delimiter> StrSplit<'haystack, 'delimiter> {
+    pub fn new(haystack: &'haystack str, delimiter: &'delimiter str) -> Self {
         Self {
             remainder: Some(haystack),
             delimiter,
@@ -16,8 +16,8 @@ impl<'a> StrSplit<'a> {
     }
 }
 
-impl<'a> Iterator for StrSplit<'a> {
-    type Item = &'a str;
+impl<'haystack, 'delimiter> Iterator for StrSplit<'haystack, 'delimiter> {
+    type Item = &'haystack str;
     fn next(&mut self) -> Option<Self::Item> {
         let remainder = self.remainder.as_mut()?;
         if let Some(next_delimiter) = remainder.find(self.delimiter) {
@@ -28,6 +28,16 @@ impl<'a> Iterator for StrSplit<'a> {
             self.remainder.take()
         }
     }
+}
+
+fn until_char<'s>(s: &'s str, c: char) -> &'s str {
+    let delimiter = format!("{}", c);
+    StrSplit::new(s, &delimiter).next().expect("StrPlit sempre retorna pelo menos um resultado")
+}
+
+#[test]
+fn until_char_test() {
+    assert_eq!(until_char("Olá mundo", 'n'), "Olá mu");
 }
 
 #[test]
